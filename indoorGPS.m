65b434a9725fe9bd92bb0bebf096fe2f60bdfce3
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2022-04-29 13:43:35 +1000

    Figures for report

--- indoorGPS.m
+++ indoorGPS.m
@@ -2,10 +2,11 @@ clc
 clear all
 close all
 
-file = "Data/oldeng2.mat";
+file = "Data/oldengrun.mat";
+measuringAlgo = "weinberg";
 
-[stepLength, timeStep] = distance_estimate(file);
-[vectorLen, x, lat, long, gyroAzimuth, azimuth, Orientationx, t] = heading_direction(file);
+[cumDistance, timeStep] = distance_estimate(file, measuringAlgo);
+[vectorLen, x, lat, long, gyroAzimuth, azimuth, Orientationx, t, K, P] = heading_direction(file);
 
 
 longditude = zeros(vectorLen,1);
@@ -13,17 +14,28 @@ latitude = zeros(vectorLen,1);
 longditude(1) = 0;
 latitude(1) = 0;
 
-j = 1;
-for i = 2:vectorLen
-    if i == timeStep(j) && j < length(timeStep)
-        j=j+1;
-        longditude(i) = longditude(i-1) + stepLength*sind(x(i));
-        latitude(i) = latitude(i-1) + stepLength*cosd(x(i));
-    else
-        longditude(i) = longditude(i-1);
-        latitude(i) = latitude(i-1);
+if measuringAlgo == "weinberg"
+    j = 1;
+    for i = 2:vectorLen
+        if i == timeStep(j) && j < length(timeStep)
+            j=j+1;
+            longditude(i) = longditude(i-1) + (cumDistance(i)-cumDistance(i-1))*sind(x(i));
+            latitude(i) = latitude(i-1) + (cumDistance(i)-cumDistance(i-1))*cosd(x(i));
+        else
+            longditude(i) = longditude(i-1);
+            latitude(i) = latitude(i-1);
+        end
+    end
+
+elseif measuringAlgo == "integrate"
+    for i = 2:vectorLen
+        longditude(i) = longditude(i-1) + (cumDistance(i)-cumDistance(i-1))*sind(x(i));
+        latitude(i) = latitude(i-1) + (cumDistance(i)-cumDistance(i-1))*cosd(x(i));
     end
 end
+        
+        
+        
 figure();
 plot(t, gyroAzimuth, 'LineWidth', 1);
 hold on 
@@ -38,12 +50,14 @@ legend('Gyro','Magnetometer','True Heading', 'Kalman Prediction')
 
 
 figure();
-% img = imread('oldeng.png');
-% img = imrotate(img,0,'bilinear','crop');
-% imagesc([-77 70], [-39 98], flipud(img));
-% 
-% 
-% hold on
+img = imread('oldeng.png');
+% %walking
+%imagesc([-77 70], [-39 98], flipud(img));
+
+%running
+imagesc([-67 42], [-34 83], flipud(img));
+
+hold on
 [trueX, trueY] = Spherical2Azimuth(lat, long, lat(1), long(1), 0, 0, 6371000*pi);
 
 plot(trueX, trueY, 'b-*','linewidth',1.5);
@@ -55,4 +69,4 @@ set(gca,'ydir','normal');
 xlabel("Latitude (m)")
 xlabel("Longitude (m)")
 legend("GNSS location", "Pedestrian Dead Reckoning")
-title("Walking Path")
+title("Running Path")
